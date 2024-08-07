625524b572c643e652103b6ce6117ebcedff1d54
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-08-07 18:32:18 +0700

    feat: add @installin and @activityscoped artis module

--- app/src/main/java/com/shaquille/tmdbclient/presentation/di/artist/ArtistModule.kt
+++ app/src/main/java/com/shaquille/tmdbclient/presentation/di/artist/ArtistModule.kt
@@ -5,10 +5,14 @@ import com.shaquille.tmdbclient.domain.usecase.UpdateArtistsUseCase
 import com.shaquille.tmdbclient.presentation.artist.ArtistViewModelFactory
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
+@InstallIn(ActivityComponent::class)
 class ArtistModule {
-    @ArtistScope
+    @ActivityScoped
     @Provides
     fun provideArtistViewModelFactory(
         getArtistsUseCase: GetArtistsUseCase,
