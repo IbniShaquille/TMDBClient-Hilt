4bd9f650da944626e7e3ecd89f219ce1ec3c5e2e
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-08-07 18:32:25 +0700

    feat: add @installin and @activityscoped movie module

--- app/src/main/java/com/shaquille/tmdbclient/presentation/di/movie/MovieModule.kt
+++ app/src/main/java/com/shaquille/tmdbclient/presentation/di/movie/MovieModule.kt
@@ -5,10 +5,14 @@ import com.shaquille.tmdbclient.domain.usecase.UpdateMoviesUsecase
 import com.shaquille.tmdbclient.presentation.movie.MovieViewModelFactory
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
+@InstallIn(ActivityComponent::class)
 class MovieModule {
-    @MovieScope
+    @ActivityScoped
     @Provides
     fun provideMovieViewModelFactory(
         getMoviesUseCase: GetMoviesUseCase,
